6226fbc01470527cf64383edde704d996bec1ee1
Martin Bakiev <mbakiev@gmail.com>
2013-02-17 13:31:28 -0500

    Updated docs

--- class_main_window.js
+++ class_main_window.js
@@ -2,5 +2,8 @@ var class_main_window =
 [
     [ "MainWindow", "class_main_window.html#a8b244be8b7b7db1b08de2a2acb9409db", null ],
     [ "~MainWindow", "class_main_window.html#ae98d00a93bc118200eeef9f9bba1dba7", null ],
+    [ "UpdateImage", "class_main_window.html#a2e6410496a1c23ecb6efa35c5929602f", null ],
+    [ "UpdatePos", "class_main_window.html#a07c9f1b8b561acfdf17cc66eb4364b61", null ],
+    [ "ft", "class_main_window.html#a5b8fc1838f90e6decb989b3c3f723b90", null ],
     [ "ui", "class_main_window.html#a35466a70ed47252a0191168126a352a5", null ]
 ];
