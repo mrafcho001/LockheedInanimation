14477fa58df0da5f70e1dfa13532952477057059
Martin Bakiev <mbakiev@gmail.com>
2013-02-17 16:44:35 -0500

    Updating docs

--- navtree.js
+++ navtree.js
@@ -2,29 +2,13 @@ var NAVTREE =
 [
   [ "Lockheed Inanimation", "index.html", [
     [ "Lockheed Inanimation Documentation Homepage", "index.html", null ],
-    [ "LockheedInanimation", "md__r_e_a_d_m_e.html", null ],
-    [ "Classes", null, [
-      [ "Class List", "annotated.html", "annotated" ],
-      [ "Class Index", "classes.html", null ],
-      [ "Class Members", "functions.html", [
-        [ "All", "functions.html", null ],
-        [ "Functions", "functions_func.html", null ],
-        [ "Variables", "functions_vars.html", null ]
-      ] ]
-    ] ],
-    [ "Files", null, [
-      [ "File List", "files.html", "files" ],
-      [ "File Members", "globals.html", [
-        [ "All", "globals.html", null ],
-        [ "Macros", "globals_defs.html", null ]
-      ] ]
-    ] ]
+    [ "LockheedInanimation", "md__r_e_a_d_m_e.html", null ]
   ] ]
 ];
 
 var NAVTREEINDEX =
 [
-"annotated.html"
+"index.html"
 ];
 
 var navTreeSubIndices = new Array();
