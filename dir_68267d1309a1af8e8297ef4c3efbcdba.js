054cec9b1bc739666c1c69574a2bdf7c764a11bd
Martin Bakiev <mbakiev@gmail.com>
2013-02-19 00:00:37 -0500

    Updated docs

--- dir_68267d1309a1af8e8297ef4c3efbcdba.js
+++ dir_68267d1309a1af8e8297ef4c3efbcdba.js
@@ -1,5 +1,8 @@
 var dir_68267d1309a1af8e8297ef4c3efbcdba =
 [
+    [ "facetracker.cpp", "facetracker_8cpp_source.html", null ],
     [ "facetracker.h", "facetracker_8h.html", "facetracker_8h" ],
+    [ "main.cpp", "main_8cpp_source.html", null ],
+    [ "mainwindow.cpp", "mainwindow_8cpp_source.html", null ],
     [ "mainwindow.h", "mainwindow_8h_source.html", null ]
 ];
