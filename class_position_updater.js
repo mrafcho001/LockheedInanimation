42a292933d0e30b6b658911dc5b86b3ed87b1f0a
Martin Bakiev <mbakiev@gmail.com>
2013-03-06 19:11:18 -0500

    Updated Logo

--- class_position_updater.js
+++ class_position_updater.js
@@ -3,6 +3,7 @@ var class_position_updater =
     [ "PositionUpdater", "class_position_updater.html#ac0c2036180065d813a08ce53f369b75b", null ],
     [ "PositionUpdater", "class_position_updater.html#a73e5e4d77eebd46d5eaaf7797f556c61", null ],
     [ "run", "class_position_updater.html#a928503fb8927ce2df061285181ee6788", null ],
-    [ "Updated", "class_position_updater.html#a24158e395fe531f6257a99533bdc89fb", null ],
+    [ "UpdateFace", "class_position_updater.html#ad2d655b59997d3419a69c258f59db4a4", null ],
+    [ "UpdateFullImage", "class_position_updater.html#a4b279c63c45c77f11dbbc53886e7839a", null ],
     [ "m_ft", "class_position_updater.html#a3132f93e25a0be7a20842c6f08f1bd95", null ]
 ];
